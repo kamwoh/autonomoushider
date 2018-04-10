fd581fa2122999981bd9df08f4ea66dc2da93440
kamwoh <kamwoh@neruti.com>
2018-04-10 13:07:58 +0800

    fixed getting cgi frame

--- rovio/rovio_video.py
+++ rovio/rovio_video.py
@@ -1,4 +1,7 @@
 import cv2
+import requests
+import numpy as np
+import urllib
 
 
 class IPCamera(object):
@@ -7,10 +10,18 @@ class IPCamera(object):
         self._frame_iter = self._get_frame_iterator()
 
     def _get_frame_iterator(self):
-        self.cap = cv2.VideoCapture(self.url)
+        stream = urllib.urlopen(self.url)
+        bytes = ''
         while True:
-            yield self.cap.read()
+            bytes += stream.read(1024)
+            a = bytes.find('\xff\xd8')
+            b = bytes.find('\xff\xd9')
+            if a != -1 and b != -1:
+                jpg = bytes[a:b + 2]
+                bytes = bytes[b + 2:]
+                frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), 1)
+                yield frame
 
     def get_frame(self):
-        ret, frame = self._frame_iter.next()
+        ret, frame = True, self._frame_iter.next()
         return ret, frame
