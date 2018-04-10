2a727c637ca144891773e10babe837389bfb61ee
kamwoh <kamwoh@neruti.com>
2018-04-10 08:25:08 +0800

    correct usage of iterator

--- rovio/rovio_video.py
+++ rovio/rovio_video.py
@@ -4,6 +4,7 @@ import cv2
 class IPCamera(object):
     def __init__(self, url):
         self.url = url
+        self._frame_iter = self._get_frame_iterator()
 
     def _get_frame_iterator(self):
         self.cap = cv2.VideoCapture(self.url)
@@ -11,5 +12,5 @@ class IPCamera(object):
             yield self.cap.read()
 
     def get_frame(self):
-        ret, frame = self._get_frame_iterator().next()
+        ret, frame = self._frame_iter.next()
         return ret, frame
